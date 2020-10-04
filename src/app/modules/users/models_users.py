0549eabdbe54cfbcef745e2433b87368ac31b442
Fanny Cacilie <fcacilie@gmail.com>
2020-10-04 18:24:02 -0300

    Add new parameters to users

--- src/app/modules/users/models_users.py
+++ src/app/modules/users/models_users.py
@@ -6,6 +6,9 @@ class UserModel(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    company_name = db.Column(db.String(40), nullable=False)
+    email = db.Column(db.String(40), nullable=False)
     username = db.Column(db.String(40), nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
 
